211911eafa114b14384f6a0c93363b01b9fa4c13
Soumya Ranjan Prusty <soumya777prusty@gmail.com>
2022-07-23 18:44:53 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -8,46 +8,19 @@ import numpy as np
 app=Flask(__name__)
 
 
-# import torch
-# import cv2
-# import time
-# import time
-# import numpy as np
-# #model = torch.hub.load('ultralytics/yolov5', 'custom',path="yolov5s.onnx",force_reload=True)
-# classes = model.names
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# print("\n\nDevice Used:",device)
-# print("-------------------------------------------")
 
-
-# def score_frame(frame):
-#         """
-#         Takes a single frame as input, and scores the frame using yolo5 model.
-#         :param frame: input frame in numpy/list/tuple format.
-#         :return: Labels and Coordinates of objects detected by model in the frame.
-#         """
-#         model.to(device)
-#         frame = [frame]
-#         results = model(frame)
-#         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
-#         # print("------------------------------------------------------------")
-#         # print(labels)
-#         # print("------------------------------------------------------------")
-#         return labels, cord
-
-
-@app.route('/', methods=['GET'])
+@app.route('/frame', methods=['POST'])
 def predict():
-    frame = request.form.get('frame')
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    print(frame)
+    # if request.method=="POST":
+    output=request.form.get("frame")
+    return jsonify({"frame":str(output)})
 
-    # result=score_frame(frame)
-
-    return jsonify(frame)
 
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
